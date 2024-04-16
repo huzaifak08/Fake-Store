14c459c5535afee362ad1a4eb6be5845e188aaa3
huzaifak08 <hk7928042@gmail.com>
2024-04-16 11:01:08 +0500

    Profile Allhamdullillah

--- lib/stores/profile_store/profile_store.dart
+++ lib/stores/profile_store/profile_store.dart
@@ -4,6 +4,7 @@ import 'package:fakes_store/services/profile_service.dart';
 import 'package:mobx/mobx.dart';
 part 'profile_store.g.dart';
 
+// ignore: library_private_types_in_public_api
 class ProfileStore = _ProfileStore with _$ProfileStore;
 
 abstract class _ProfileStore with Store {
